e7f86c992ebdd034cdaaabedd07a38166be87bc6
francessco410 <francessco410@gmail.com>
2017-06-14 15:34:09 +0100

    Bootstrap + Ajax

--- js/favorites.js
+++ js/favorites.js
@@ -1,4 +1,11 @@
 $('.checkbox').click(function(){
     $(this).toggleClass('is-checked');
-    
+    $.ajax({
+        type : "POST",
+        url : "scripts/favorite.script.php",
+        data : {object_id: 2},
+        success : function(){
+            //document.write('success');
+        }
+    });
 }); 
